d0a1815ca5dff568989b125e0381a2ec88e3e886
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-12 15:54:12 -0700

    almost done with frontend and backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import TipBtnComponent from "./Components/TipBtnComponent"
 
 
 
 function App() {
-  const [customTip, setCustomTip] = useState()
   const [tip, setTip] = useState()
   const[bill, setBill] = useState()
   const[people, setPeople] = useState()
+  const [tipAmount, setTipAmount] = useState(0)
+  const [total, setTotal] = useState(0)
 
 useEffect(() => {
   console.log(tip)
@@ -18,32 +19,65 @@ useEffect(() => {
      <div className='bg-[#c5e4e7] w-screen h-screen flex flex-col items-center justify-center'>
       <img className='h-[48px] w-[80px] mb-20' src="../images/logo.svg" alt="the splitter logo" />
 
-      <div className='bg-white w-[800px] h-auto rounded-2xl flex flex-col items-center justify-center'>
+      <div className='bg-white md:w-[800px] h-auto rounded-2xl flex flex-col md:flex-row items-center p-10 justify-center'>
 
-        <div className="w-full h-auto flex flex-col md:flex-row items-center ">
+       
 
-          <div className="w-full h-auto flex flex-col items-center">
+          <div className="w-full h-auto flex flex-col items-center me-2">
 
-            <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium">Select Tip %</p>
-            <div className="w-full h-auto flex flex-row justify-between pb-3">
-              <TipBtnComponent text={5} tipSetter={setTip} />
-              <TipBtnComponent text={10} tipSetter={setTip} />
-              <TipBtnComponent text={15} tipSetter={setTip} />
+          <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium">Bill</p>
+          <div className="w-full h-auto flex flex-row items-center bg-[#f4fafa] px-2 rounded-lg hover:border-4 hover:border-[#26c0ab] mb-2">
+            <img src="../images/icon-dollar.svg" alt="a dollar sign"/>
+            <input  onChange={(e) => setBill(e.target.value)} type="text" placeholder="0" className=" text-end h-[60px] w-full text-[24px] font-bold bg-[#f4fafa] text-[#00494d] flex items-center justify-center" />
+          </div>
+
+                      <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium row-start-1">Select Tip %</p>
+            <div className="w-full h-auto justify-between pb-3 grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 gap-2">
+              <TipBtnComponent text={5} tipSetter={setTip} extraClass={"col-start-1 row-start-1 md:col-start-1 md:row-start-1"}/>
+              <TipBtnComponent text={10} tipSetter={setTip} extraClass={"col-start-2 row-start-1 md:col-start-2 md:row-start-1"}/>
+              <TipBtnComponent text={15} tipSetter={setTip} extraClass={"col-start-1 row-start-2 md:col-start-3 md:row-start-1"}/>
+              <TipBtnComponent text={25} tipSetter={setTip} extraClass={"col-start-2 row-start-2 md:col-start-1 md:row-start-2"}/>
+              <TipBtnComponent text={50} tipSetter={setTip} extraClass={"col-start-1 row-start-3 md:col-start-2 md:row-start-2"}/>
+              <input  onChange={(e) => setTip(e.target.value)} type="text" placeholder="Custom" className=" text-center h-[60px] w-[90%] text-[24px] font-bold bg-[#f4fafa] text-[#00494d] hover:border-4 hover:border-[#26c0ab] rounded-lg flex items-center justify-center col-start-2 row-start-3 md:col-start-3 md:row-start-2" />
             </div>
-            <div className="w-full h-auto flex flex-row justify-between">
-              <TipBtnComponent text={25} tipSetter={setTip} />
-              <TipBtnComponent text={50} tipSetter={setTip} />
-              <input  onChange={(e) => setTip(e.target.value)} type="text" placeholder="Custom" className=" text-center h-[60px] w-[125px] text-[24px] font-bold bg-[#f4fafa] text-[#00494d] hover:border-4 hover:border-[#26c0ab] rounded-lg flex items-center justify-center" />
-            </div>
 
+            <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium">Number of people</p>
+          <div className="w-full h-auto flex flex-row items-center bg-[#f4fafa] px-2 rounded-lg hover:border-4 hover:border-[#26c0ab] mb-2">
+            <img src="../images/icon-person.svg" alt="a dollar sign"/>
+            <input  onChange={(e) => setPeople(e.target.value)} type="text" placeholder="0" className=" text-end h-[60px] w-full text-[24px] font-bold bg-[#f4fafa] text-[#00494d] flex items-center justify-center" />
+          </div>
           
           </div>
-
-          <div className="w-full h-auto flex flex-col items-center">
+  
+        <div className="w-full h-auto md:h-full rounded-2xl flex flex-col items-center ms-2 bg-[#00494d]">
             
+          <div className="w-[80%] flex justify-between items-center mt-12">
+
+            <div>
+              <h2 className="text-[16px] text-white">Tip Amount</h2>
+              <p className="text-[14px] text-[#5e7a7d]">/ people</p>
+            </div>
+
+            <p className="text-[30px] text-[#26c0ab] font-medium">${tipAmount}</p>
+
           </div>
 
+          <div className="w-[80%] flex justify-between items-center mt-4">
+
+          <div>
+            <h2 className="text-[16px] text-white">Total</h2>
+            <p className="text-[14px] text-[#5e7a7d]">/ people</p>
+          </div>
+
+          <p className="text-[30px] text-[#26c0ab] font-medium">${total}</p>
+
+          </div>
+
+        
+
         </div>
+
+        
 
         
 
--- src/Components/TipBtnComponent.jsx
+++ src/Components/TipBtnComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 
 const TipBtnComponent = (props) => {
+  const [style, setStyle] = React.useState(`h-[60px] w-[90%] bg-[#00494d] text-[#f4fafa] hover:bg-[#26c0ab] hover:text-[#00494d] rounded-lg flex items-center justify-center ${props.extraClass}`)
 
   const doThing = () => {
     props.tipSetter(props.text)
@@ -9,7 +10,7 @@ const TipBtnComponent = (props) => {
 
 
   return (
-    <div onClick={doThing} className='h-[60px] w-[125px] bg-[#00494d] text-[#f4fafa] hover:bg-[#26c0ab] hover:text-[#00494d] rounded-lg flex items-center justify-center'>
+    <div onClick={doThing} className={style}>
 
         <span className='text-[24px] font-medium'>{`${props.text}%`}</span>
         
