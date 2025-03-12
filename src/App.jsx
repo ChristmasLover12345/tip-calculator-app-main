f83b82885c1d19690f1e0cc40208e606879b0bae
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-12 14:03:11 -0700

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { use, useEffect, useState } from "react"
 import TipBtnComponent from "./Components/TipBtnComponent"
 
 
 
 function App() {
-  
+  const [customTip, setCustomTip] = useState()
+  const [tip, setTip] = useState()
+  const[bill, setBill] = useState()
+  const[people, setPeople] = useState()
+
+useEffect(() => {
+  console.log(tip)
+},[tip])
 
   return (
     <>
@@ -12,7 +20,32 @@ function App() {
 
       <div className='bg-white w-[800px] h-auto rounded-2xl flex flex-col items-center justify-center'>
 
-        <TipBtnComponent text="20%" />
+        <div className="w-full h-auto flex flex-col md:flex-row items-center ">
+
+          <div className="w-full h-auto flex flex-col items-center">
+
+            <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium">Select Tip %</p>
+            <div className="w-full h-auto flex flex-row justify-between pb-3">
+              <TipBtnComponent text={5} tipSetter={setTip} />
+              <TipBtnComponent text={10} tipSetter={setTip} />
+              <TipBtnComponent text={15} tipSetter={setTip} />
+            </div>
+            <div className="w-full h-auto flex flex-row justify-between">
+              <TipBtnComponent text={25} tipSetter={setTip} />
+              <TipBtnComponent text={50} tipSetter={setTip} />
+              <input  onChange={(e) => setTip(e.target.value)} type="text" placeholder="Custom" className=" text-center h-[60px] w-[125px] text-[24px] font-bold bg-[#f4fafa] text-[#00494d] hover:border-4 hover:border-[#26c0ab] rounded-lg flex items-center justify-center" />
+            </div>
+
+          
+          </div>
+
+          <div className="w-full h-auto flex flex-col items-center">
+            
+          </div>
+
+        </div>
+
+        
 
       </div>
 
--- src/Components/TipBtnComponent.jsx
+++ src/Components/TipBtnComponent.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const TipBtnComponent = (props) => {
-  return (
-    <div className='h-[60px] w-[125px] bg-[#00494d] text-[#f4fafa] hover:bg-[#26c0ab] hover:text-[#00494d] rounded-lg flex items-center justify-center'>
 
-        <span className='text-[24px] font-medium'>{props.text}</span>
+  const doThing = () => {
+    props.tipSetter(props.text)
+  }
+
+
+
+  return (
+    <div onClick={doThing} className='h-[60px] w-[125px] bg-[#00494d] text-[#f4fafa] hover:bg-[#26c0ab] hover:text-[#00494d] rounded-lg flex items-center justify-center'>
+
+        <span className='text-[24px] font-medium'>{`${props.text}%`}</span>
         
     </div>
   )
