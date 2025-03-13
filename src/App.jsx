b318734e6ae1732146058140778e2bd403dec0b8
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-13 16:05:36 -0700

    Managed to figure out how to habe buttons stay selected, currently trying to figure out how to reset the input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import './App.css'
 import TipBtnComponent from "./Components/TipBtnComponent"
 
 
@@ -7,8 +8,15 @@ function App() {
   const [tip, setTip] = useState()
   const[bill, setBill] = useState()
   const[people, setPeople] = useState()
-  const [tipAmount, setTipAmount] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [tipAmount, setTipAmount] = useState(4.27)
+  const [total, setTotal] = useState(32.79)
+
+
+  const [clicked1, setClicked1] = useState(false)
+  const [clicked2, setClicked2] = useState(false)
+  const [clicked3, setClicked3] = useState(false)
+  const [clicked4, setClicked4] = useState(false)
+  const [clicked5, setClicked5] = useState(false)
 
 useEffect(() => {
   console.log(tip)
@@ -16,10 +24,10 @@ useEffect(() => {
 
   return (
     <>
-     <div className='bg-[#c5e4e7] w-screen h-screen flex flex-col items-center justify-center'>
+     <div className='bg-[#c5e4e7] w-screen h-screen flex flex-col items-center justify-center '>
       <img className='h-[48px] w-[80px] mb-20' src="../images/logo.svg" alt="the splitter logo" />
 
-      <div className='bg-white md:w-[800px] h-auto rounded-2xl flex flex-col md:flex-row items-center p-10 justify-center'>
+      <div className='bg-white md:w-[90%] lg:w-[900px] h-auto rounded-2xl flex flex-col md:flex-row items-center p-10 justify-center'>
 
        
 
@@ -33,18 +41,18 @@ useEffect(() => {
 
                       <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium row-start-1">Select Tip %</p>
             <div className="w-full h-auto justify-between pb-3 grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 gap-2">
-              <TipBtnComponent text={5} tipSetter={setTip} extraClass={"col-start-1 row-start-1 md:col-start-1 md:row-start-1"}/>
-              <TipBtnComponent text={10} tipSetter={setTip} extraClass={"col-start-2 row-start-1 md:col-start-2 md:row-start-1"}/>
-              <TipBtnComponent text={15} tipSetter={setTip} extraClass={"col-start-1 row-start-2 md:col-start-3 md:row-start-1"}/>
-              <TipBtnComponent text={25} tipSetter={setTip} extraClass={"col-start-2 row-start-2 md:col-start-1 md:row-start-2"}/>
-              <TipBtnComponent text={50} tipSetter={setTip} extraClass={"col-start-1 row-start-3 md:col-start-2 md:row-start-2"}/>
+              <TipBtnComponent text={5} tipSetter={setTip} switchCheck={tip} extraClass={"col-start-1 row-start-1 md:col-start-1 md:row-start-1"}/>
+              <TipBtnComponent text={10} tipSetter={setTip} switchCheck={tip} extraClass={"col-start-2 row-start-1 md:col-start-2 md:row-start-1"}/>
+              <TipBtnComponent text={15} tipSetter={setTip} switchCheck={tip} extraClass={"col-start-1 row-start-2 md:col-start-3 md:row-start-1"}/>
+              <TipBtnComponent text={25} tipSetter={setTip} switchCheck={tip} extraClass={"col-start-2 row-start-2 md:col-start-1 md:row-start-2"}/>
+              <TipBtnComponent text={50} tipSetter={setTip} switchCheck={tip} extraClass={"col-start-1 row-start-3 md:col-start-2 md:row-start-2"}/>
               <input  onChange={(e) => setTip(e.target.value)} type="text" placeholder="Custom" className=" text-center h-[60px] w-[90%] text-[24px] font-bold bg-[#f4fafa] text-[#00494d] hover:border-4 hover:border-[#26c0ab] rounded-lg flex items-center justify-center col-start-2 row-start-3 md:col-start-3 md:row-start-2" />
             </div>
 
             <p className="text-start self-start text-[#5e7a7d] mb-3 text-[18px] font-medium">Number of people</p>
           <div className="w-full h-auto flex flex-row items-center bg-[#f4fafa] px-2 rounded-lg hover:border-4 hover:border-[#26c0ab] mb-2">
             <img src="../images/icon-person.svg" alt="a dollar sign"/>
-            <input  onChange={(e) => setPeople(e.target.value)} type="text" placeholder="0" className=" text-end h-[60px] w-full text-[24px] font-bold bg-[#f4fafa] text-[#00494d] flex items-center justify-center" />
+            <input onBlur={(e) => (e.target.value = "")}  onChange={(e) => setPeople(e.target.value)} type="text" placeholder="0" className=" text-end h-[60px] w-full text-[24px] font-bold bg-[#f4fafa] text-[#00494d] flex items-center justify-center" />
           </div>
           
           </div>
@@ -58,7 +66,7 @@ useEffect(() => {
               <p className="text-[14px] text-[#5e7a7d]">/ people</p>
             </div>
 
-            <p className="text-[30px] text-[#26c0ab] font-medium">${tipAmount}</p>
+            <p className="text-[38px] text-[#26c0ab] font-medium">${tipAmount}</p>
 
           </div>
 
@@ -69,8 +77,12 @@ useEffect(() => {
             <p className="text-[14px] text-[#5e7a7d]">/ people</p>
           </div>
 
-          <p className="text-[30px] text-[#26c0ab] font-medium">${total}</p>
+          <p className="text-[38px] text-[#26c0ab] font-medium">${total}</p>
 
+          </div>
+
+          <div className="w-[80%] h-[60px] bg-[#26c0ab] rounded-2xl ">
+            
           </div>
 
         
--- src/Components/TipBtnComponent.jsx
+++ src/Components/TipBtnComponent.jsx
@@ -1,18 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TipBtnComponent = (props) => {
-  const [style, setStyle] = React.useState(`h-[60px] w-[90%] bg-[#00494d] text-[#f4fafa] hover:bg-[#26c0ab] hover:text-[#00494d] rounded-lg flex items-center justify-center ${props.extraClass}`)
+  const [style, setStyle] = useState()
 
   const doThing = () => {
     props.tipSetter(props.text)
   }
 
+useEffect(() => { 
 
+
+  const switchFunc = () => {
+  if (props.text === props.switchCheck)
+  { 
+    setStyle(`h-[60px] w-[90%] hover:bg-[#c5e4e7] bg-[#26c0ab] text-[#00494d] hover:text-[#00494d] rounded-lg flex items-center justify-center ${props.extraClass}`)
+  }
+  else
+  {
+    setStyle(`h-[60px] w-[90%] bg-[#00494d] text-[#f4fafa] hover:bg-[#c5e4e7] hover:text-[#00494d] rounded-lg flex items-center justify-center ${props.extraClass}`)  
+  }
+
+}
+
+switchFunc()
+
+  
+
+ }, [props.switchCheck])
 
   return (
     <div onClick={doThing} className={style}>
 
-        <span className='text-[24px] font-medium'>{`${props.text}%`}</span>
+        <span className='text-[24px] font-medium '>{`${props.text}%`}</span>
         
     </div>
   )
